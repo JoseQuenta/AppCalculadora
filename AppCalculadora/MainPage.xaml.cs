8b49bef358521a9709097163d5bd2ab86d1c203a
JOSE ANTONIO QUENTA RAMOS <1379908@senati.pe>
2023-09-25 13:10:28 -0500

    2° Commit - Nos quedamos declarando las variables globales

--- AppCalculadora/MainPage.xaml.cs
+++ AppCalculadora/MainPage.xaml.cs
@@ -15,19 +15,50 @@ namespace AppCalculadora
             InitializeComponent();
         }
 
+        private decimal numberone;
+        private string operador;
+        private bool SeleccionadorOperador;
+
+        // JOSÉ QUENTA --> Aqui me quede ....
         private void BtnClear_Clicked(object sender, EventArgs e)
         {
+            string number = lblResult.Text;
+            if (number != "0")
+            {
+                number = number.Remove(number.Length - 1);
+                if (string.IsNullOrEmpty(number))
+                {
+                    lblResult.Text = "0";
+                }
+                else
+                {
 
+                    lblResult.Text = number;
+                }
+            }
         }
 
         private void BtnEliminar_Clicked(object sender, EventArgs e)
         {
-
+            lblResult.Text = "0";
         }
 
         private void BtnPorcentaje_Clicked(object sender, EventArgs e)
         {
-
+            try
+            {
+                string number = lblResult.Text;
+                if (number != "0")
+                {
+                    decimal numeroDecimal = Convert.ToDecimal(number);
+                    string result = (numeroDecimal / 100).ToString("0.##");
+                    lblResult.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "Ok");
+            }
         }
 
         private void BtnDividir_Clicked(object sender, EventArgs e)
@@ -48,6 +79,38 @@ namespace AppCalculadora
         private void BtnSumar_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private void Digitar_Clicked(object sender, EventArgs e)
+        {
+            //lblResult.Text = "7";
+            var button = sender as Button;
+
+            if (lblResult.Text.Length < 10)
+            {
+                if (lblResult.Text == "0")
+                {
+                    lblResult.Text = button.Text;
+                }
+                else
+                {
+                    lblResult.Text += button.Text;
+                }
+            }
+
+        }
+
+        private void DigitarPunto_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (lblResult.Text.StartsWith("0") && lblResult.Text.Length == 1)
+            {
+                lblResult.Text = "0" + button.Text;
+            }
+            else
+            {
+                lblResult.Text = button.Text;
+            }
         }
     }
 }
