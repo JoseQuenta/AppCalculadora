4b4fdc724c6db5c3f9e1d159cc3039b2349fb1a2
Laboratorio 3-305 <Laboratorio 3-305@PC01-3305>
2023-09-25 10:39:09 -0500

    Add project files.

---
+++ AppCalculadora/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEliminar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPorcentaje_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRestar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSumar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
